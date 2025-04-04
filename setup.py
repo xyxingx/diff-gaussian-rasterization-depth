694fc79735bcb21624af636bb3f471c21b318058
Xiaoyan XING <52537117+xyxingx@users.noreply.github.com>
2025-04-04 13:42:26 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,19 +15,27 @@ import os
 os.path.dirname(os.path.abspath(__file__))
 
 setup(
-    name="diff_gaussian_rasterization",
-    packages=['diff_gaussian_rasterization'],
+    name="diff_gaussian_rasterization_depth",  # <-- New package name
+    packages=['diff_gaussian_rasterization_depth'],  # <-- New package folder
     ext_modules=[
         CUDAExtension(
-            name="diff_gaussian_rasterization._C",
+            name="diff_gaussian_rasterization_depth._C",  # <-- also rename this
             sources=[
-            "cuda_rasterizer/rasterizer_impl.cu",
-            "cuda_rasterizer/forward.cu",
-            "cuda_rasterizer/backward.cu",
-            "rasterize_points.cu",
-            "ext.cpp"],
-            extra_compile_args={"nvcc": ["-I" + os.path.join(os.path.dirname(os.path.abspath(__file__)), "third_party/glm/")]})
-        ],
+                "cuda_rasterizer/rasterizer_impl.cu",
+                "cuda_rasterizer/forward.cu",
+                "cuda_rasterizer/backward.cu",
+                "rasterize_points.cu",
+                "ext.cpp"],
+            extra_compile_args={
+                "nvcc": [
+                    "-I" + os.path.join(
+                        os.path.dirname(os.path.abspath(__file__)), 
+                        "third_party/glm/"
+                    )
+                ]
+            }
+        )
+    ],
     cmdclass={
         'build_ext': BuildExtension
     }
